318aad13235d81050e85da594399927e38e0aa97
smeyer <smeyer.ilias@gmx.de>
2020-07-21 16:12:54 +0200

    Increased plugin version

--- plugin.php
+++ plugin.php
@@ -3,11 +3,11 @@
 $id = "vedaclaiming";
 
 // code version; must be changed for all code changes
-$version = "5.4.4.4";
+$version = "6.1.0";
 
 // ilias min and max version; must always reflect the versions that should run with the plugin
-$ilias_min_version = "5.4.1";
-$ilias_max_version = "5.4.99";
+$ilias_min_version = "6.1";
+$ilias_max_version = "6.99";
 
 // optional, but useful: Add one or more responsible persons and a contact email
 $responsible = 'Stefan Meyer';
