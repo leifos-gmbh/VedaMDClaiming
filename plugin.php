d3c9b43d89f45ca2e80d82a8555a4abb06c09a8a
smeyer <smeyer.ilias@gmx.de>
2021-11-11 11:28:53 +0100

    r6 support

--- plugin.php
+++ plugin.php
@@ -6,7 +6,7 @@ $id = "vedaclaiming";
 $version = "7.1.1";
 
 // ilias min and max version; must always reflect the versions that should run with the plugin
-$ilias_min_version = "7.1";
+$ilias_min_version = "6.1";
 $ilias_max_version = "7.99";
 
 // optional, but useful: Add one or more responsible persons and a contact email
